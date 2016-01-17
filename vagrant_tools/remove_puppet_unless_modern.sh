19cabb24a8e90432408a63ca78559e1246d36705
Ryan Wilcox <rwilcox@wilcoxd.com>
2016-01-16 20:24:40 -0500

    better working "remove puppet unless modern" script

--- vagrant_tools/remove_puppet_unless_modern.sh
+++ vagrant_tools/remove_puppet_unless_modern.sh
@@ -1,17 +1,20 @@
 #!/usr/bin/env bash
 #
 # This removes ancient Puppet versions on the VM - if there IS any ancient
-# version on it - so we can install the latest.
+# version on it - so we can install the latest (Puppet 4.3).
 #
 # It is meant to be run as part of a provisioning run by Vagrant
 # so it must ONLY delete old versions (not current versions other stages have installed)
 #
-# It assumes that we're targeting Puppet 3.7 (modern as of Feb 2015...)
 
-INSTALLED_PUPPET_VERSION=$(apt-cache policy puppet | grep "Installed: " | cut -d ":" -f 2 | xargs)
+
+if [[ -x /opt/puppetlabs/puppet/bin/puppet ]]; then
+  INSTALLED_PUPPET_VERSION=$(/opt/puppetlabs/puppet/bin/puppet --version | xargs)
+fi
+
 echo "Currently installed version: $INSTALLED_PUPPET_VERSION"
 
-if [[ $INSTALLED_PUPPET_VERSION != 3.7* ]] ; then
+if [[ $INSTALLED_PUPPET_VERSION && $INSTALLED_PUPPET_VERSION != 4.3* ]] ; then
   apt-get remove -y puppet=$INSTALLED_PUPPET_VERSION puppet-common=$INSTALLED_PUPPET_VERSION
   echo "Removed old Puppet version: $INSTALLED_PUPPET_VERSION"
 fi
